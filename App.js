4750365d25d3489e99452730e7ee2bc3f05cd442
Robert Niyitanga <niroberto>
2024-01-15 16:16:19 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import navigationTheme from "./app/navigator/navigationTheme";
+import OnBoarding from "./app/navigator/OnBoardingNavigator";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer theme={navigationTheme}>
       <StatusBar style="auto" />
-    </View>
+      <OnBoarding />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
